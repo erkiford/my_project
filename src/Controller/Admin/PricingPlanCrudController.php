a7e35d0da3605541011479ef378a02455251d4d3
erkiford <45500895+erkiford@users.noreply.github.com>
2023-03-05 17:18:10 +0100

    problems with easy admin

--- src/Controller/Admin/PricingPlanCrudController.php
+++ src/Controller/Admin/PricingPlanCrudController.php
@@ -2,24 +2,14 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\PricingPlan;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use App\Entity\PricingPlan;
 
 class PricingPlanCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
-    {
-        return PricingPlan::class;
-    }
-
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+public static function getEntityFqcn(): string
+{
+return PricingPlan::class;
 }
+}
+
