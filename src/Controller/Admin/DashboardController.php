7cc4a96eb26cb1bb0b8e0ae87fbd80ed28c6b6ba
erkiford <45500895+erkiford@users.noreply.github.com>
2023-03-06 03:35:22 +0100

    trying a different approach
    gives exception error

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,27 +2,38 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\PricingPlan;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-/**
-* @Route("/admin", name="admin")
-*/
-public function index(): Response
-{
-return parent::index();
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('My Admin Dashboard');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Pricing Plans');
+        yield MenuItem::linkToCrud('PricingPlan', 'fa fa-dollar-sign', PricingPlan::class);
+        // Add more entities to the dashboard here
+    }
 }
 
-public function configureMenuItems(): iterable
-{
-yield from [
-['label' => 'Dashboard', 'route' => 'admin'],
-['label' => 'Pricing Plans', 'route' => 'admin_app_pricingplan_index'],
-];
-}
-}
+
 
 
