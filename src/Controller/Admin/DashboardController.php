8b14a251a61e7607cb86b41455b3ad40efa8d87b
erkiford <45500895+erkiford@users.noreply.github.com>
2023-03-10 13:58:43 +0100

    did easyadmin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,37 +3,44 @@
 namespace App\Controller\Admin;
 
 use App\Entity\PricingPlan;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use phpDocumentor\Reflection\Types\Iterable_;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
-    /**
-     * @Route("/admin", name="admin")
-     */
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-    }
+        return $this->render('admin/index.html.twig');
 
-    public function configureDashboard(): Dashboard
-    {
-        return Dashboard::new()
-            ->setTitle('My Admin Dashboard');
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::section('Pricing Plans');
-        yield MenuItem::linkToCrud('PricingPlan', 'fa fa-dollar-sign', PricingPlan::class);
-        // Add more entities to the dashboard here
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+
+            MenuItem::section('Pricing'),
+            MenuItem::linkToCrud('PricingPlans', 'fa fa-tags', PricingPlan::class),
+        ];
     }
 }
-
-
-
-
--- src/Controller/Admin/PricingPlanCrudController.php
+++ src/Controller/Admin/PricingPlanCrudController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use App\Entity\PricingPlan;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PricingPlanCrudController extends AbstractCrudController
 {
-public static function getEntityFqcn(): string
-{
-return PricingPlan::class;
-}
-}
+    public static function getEntityFqcn(): string
+    {
+        return PricingPlan::class;
+    }
 
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
